089be55dbee4804d8b349ac2993b39a21428bea8
Shraddha <mailmeatshraddha@gmail.com>
2021-09-03 19:30:06 -0400

    feature: setup music player container ui

---
+++ projects/music-player/scripts/tracks.js
@@ -0,0 +1,27 @@
+const tracks = [
+  {
+    title: "In Christ Alone",
+    artist: "Shane Barnard",
+    url: "../audio/shane-n-shane-in-christ-alone.mp3"
+  },
+  {
+    title: "The King of Love",
+    artist: "Stephanie Kulla",
+    url: "../audio/stephanie-kulla-king-of-love.mp3"
+  },
+  {
+    title: "Señor, te necesito",
+    artist: "Blanca & Matt Meher",
+    url: "../audio/matt-maher-blanca-lord-i-need-you.mp3"
+  },
+  {
+    title: "Hope for Everyone",
+    artist: "Matt Meher",
+    url: "../audio/matt-maher-hope-for-everyone.mp3"
+  },
+  {
+    title: "The Wonderful Cross",
+    artist: "Chris Tomlin & Matt Redman",
+    url: "../audio/chris-tomlin-n-matt-redman-the-wonderful-cross.mp3"
+  },
+];
